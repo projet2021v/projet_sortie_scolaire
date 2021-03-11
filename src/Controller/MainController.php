a321c14818ec0ea929048d9831fb5ee5121f2fba
gilles8 <gilles.yzambart2020@campus-eni.fr>
2021-03-11 16:37:13 +0100

    ajout ihm recherche

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Site;
 use App\Entity\Sortie;
+use App\Repository\SiteRepository;
 use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,12 +15,14 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function home(SortieRepository $repo): Response
+    public function home(SortieRepository $repo, SiteRepository $repoSite): Response
     {
-        //$sortie = new Sortie();
+
+        $sites = $repoSite->findAll();
         $sorties = $repo->findAll();
         return $this->render('main/index.html.twig', [
             'sorties' => $sorties,
+            'sites' => $sites,
         ]);
     }
 }
